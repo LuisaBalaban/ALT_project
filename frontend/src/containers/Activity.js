f289539c2fd98d82ae8225a3915de181fada3e12
LuisaBalaban <67561596+LuisaBalaban@users.noreply.github.com>
2020-12-05 21:19:18 +0200

    faviconupd

--- frontend/src/containers/Activity.js
+++ frontend/src/containers/Activity.js
@@ -7,11 +7,17 @@ function Activity()
     return(<div>
         <Header/>
         <h3>Create a new activity</h3>
-        <div className="CreateAct">
+        <div className="Register">
 
-            <label for="class"  id="class" >Class: </label>
+            <label className="label" for="class"  id="class" >Class: </label>
             <input type="text" id="uname" name="uname" value="Linear algebra"/><br/>
+            <label className="label" for="date"  id="date" >Date: </label>
+            <input type="date" id="date" name="date" value="10/09/2020"/><br/>
+            <label className="label" for="time"  id="time" >Time: </label>
+            <input type="time" id="time" name="time" value="10:20"/><br/>
             </div>
+         <button type="submit" className="msgBtn" onClick="return false;">Generate code</button><br/>
+         <input type="text" id="code" name="code" value="2x81J2"/><br/>
       
         </div>)
 }
