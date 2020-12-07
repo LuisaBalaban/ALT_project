4d05b162d316b612fa3c117dbb3ebcdcd5fe43f8
LuisaBalaban <67561596+LuisaBalaban@users.noreply.github.com>
2020-12-07 19:13:11 +0200

    'msg'

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import './components.css';
+import { useHistory } from "react-router-dom";
 
 function Form()
 {
+  const history = useHistory();
+  const routeChangeRegister = () =>{ 
+    let path = `/register`; 
+    history.push(path);
+  }
+  const routeChangeSign = () =>{ 
+    let path = `/TeacherMain`; 
+    history.push(path);
+  }
+
     return(
       <div className="Form">
            <div className="radio">
-           <input type="radio" id="Student" name="type" value="Student"/>
+           <input type="radio" id="Student" name="type" value="Student" />
            <label for="Student">Student</label>
       
-           <input type="radio" id="Teacher" name="type" value="Teacher"/>
+           <input type="radio" id="Teacher" name="type" value="Teacher" />
             <label for="Student">Teacher</label>
             </div>
            <div className="form-insert">
@@ -21,8 +32,8 @@ function Form()
             
             </div>
            <div className="form-send">
-          <button type="submit" className="msgBtn" onClick="return false;">Sign In</button>
-          <button type="submit" className="msgBtn" onClick="return false;">Register</button>
+          <button type="submit" className="msgBtn" onClick={routeChangeSign}>Sign In</button>
+          <button type="submit" className="msgBtn" onClick={routeChangeRegister}>Register</button>
           </div>
         </div>
         
--- frontend/src/containers/Register.js
+++ frontend/src/containers/Register.js
@@ -3,16 +3,32 @@ import '../components/components.css';
 
 
 class Register extends Component {
+
+  constructor() {
+    super()
+    this.state = {}
+    this.onChangeValue = this.onChangeValue.bind(this)
+  }
+
+  onChangeValue(event) {
+    this.setState({
+      selectedOption: event.target.value
+      
+    });
+    console.log(event.target.value);
+  }
+
     render() {
       return (
         <div>
         <h3>Create an account!</h3>
         <div className="Register">
-        <div id="radio">
-           <input type="radio" id="Student" name="type" value="Student"/>
+        
+        <div id="radio" onChange={this.onChangeValue}>
+           <input type="radio" id="Student" name="type" value="Student" checked={this.state.selectedOption === "Student"}/>
            <label for="Student">Student</label>
       
-           <input type="radio" id="Teacher" name="type" value="Teacher"/>
+           <input type="radio" id="Teacher" name="type" value="Teacher" checked={this.state.selectedOption === "Teacher"}/>
             <label for="Student">Teacher</label><br/>
             </div>
 
@@ -24,7 +40,7 @@ class Register extends Component {
             <input  className="input" type="password" id="pass" name="pass" value="abc"/><br/>
             <label className="label"  for="email"  id="email">Email: </label>
             <input  className="input" type="email" id="email" name="email" value="email@abc.com"/><br/>
-            <button type="submit" className="msgBtn" onClick="return false;">Register</button>
+            <button type="submit" className="msgBtn">Register</button>
             </div>
       
         </div>)
