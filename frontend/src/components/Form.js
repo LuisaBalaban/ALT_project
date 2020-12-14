ee2f056d2f9ff27f7b2772e3f48b7211747bc0f7
LuisaBalaban <67561596+LuisaBalaban@users.noreply.github.com>
2020-12-15 00:22:48 +0200

    update post

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './components.css';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios'
 
 class Form extends React.Component{
 constructor()
@@ -14,6 +15,7 @@ constructor()
   }
   this.handleChange=this.handleChange.bind(this)
   this.routeChange = this.routeChange.bind(this);
+  this.onSend=this.routeChange.bind(this)
 
 }
 // onSubmit()
@@ -25,37 +27,64 @@ constructor()
 // }
 handleChange(event) {
   const {name, value, type, checked} = event.target
-  type === "radio" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+//  type === "radio" ? this.setState({ [name]: checked }) :
+   this.setState({ [name]: value })
 }
 routeChange() {
   let path = `/register`;
   this.props.history.push(path);
 }
+onSend()
+{
+  const formData = {
+    id: this.state.id,
+    password: this.state.password,
+    username: this.state.username
+    }
+
+   axios({
+    method: 'get',
+    url: 'http://localhost:3000/api/auth/login',
+    data: formData,
+    config: { headers: {'Content-Type': 'multipart/form-data' }}
+   })
+   .then(function (response) {
+   
+  console.log(response);
+   })
+.catch(function (response) {
+  console.log(response);
+
+});
+}
 
 render(){
 
     return(
+      <div>
       <form className="Form">
            <div className="radio">
-           <input type="radio" id="Student" name="id" value="Student"  checked={this.state.id === "1"} onChange={this.handleChange}/>
-           <label for="Student">Student</label>
+           <input type="radio" id="Student" name="id" checked={this.state.id === "1"} value="1"  onChange={this.handleChange}/>
+           <label>Student</label>
       
-           <input type="radio" id="Teacher" name="id" value="Teacher" checked={this.state.id === "2"} onChange={this.handleChange} />
-            <label for="Teacher" >Teacher</label>
+           <input type="radio" id="Teacher" name="id" checked={this.state.id === "2"} value="2" onChange={this.handleChange} />
+            <label>Teacher</label>
             </div>
            <div className="form-insert">
            
-            <label for="uname"  id="uname" >Username: </label>
-            <input type="text" id="uname" name="username" value={this.state.username} onChange={this.handleChange}/><br/>
-            <label for="pass"  id="uname">Password: </label>
+            <label id="username" >Username: </label>
+            <input type="text" id="username" name="username" value={this.state.username} onChange={this.handleChange}/><br/>
+            <label>Password: </label>
             <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleChange}/>
             
             </div>
-           <div className="form-send">
-          <button type="submit" className="msgBtn" >Sign In</button>
+            </form>
+           <div className="form-send"> 
+          <button className="msgBtn" onClick={this.onSend} >Sign In</button>
           <button className="msgBtn" onClick={this.routeChange}>Register</button>
           </div>
-        </form>
+          </div>
+     
         
       
     )
--- frontend/src/containers/Register.js
+++ frontend/src/containers/Register.js
@@ -1,6 +1,7 @@
 import React,{ Component } from 'react'
 import { withRouter } from 'react-router-dom';
 import '../components/components.css';
+import axios from 'axios'
 
 
 class Register extends Component {
@@ -17,16 +18,40 @@ class Register extends Component {
     }
     this.handleChange = this.handleChange.bind(this)
     this.routeChange=this.routeChange.bind(this)
+    this.onSubmit=this.onSubmit.bind(this)
   }
 
   handleChange(event) {
     const {name, value, type, checked} = event.target
-    type === "radio" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+     this.setState({ [name]: value })
   }
   routeChange() {
     let path = "/";
     this.props.history.push(path);
   }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const userObject = {
+      id:this.state.id,
+      username:this.state.username,
+      password:this.state.password,
+        name: this.state.name,
+        email: this.state.email
+    };
+
+    axios.post('http://localhost:3000/api/auth/register', userObject)
+        .then((res) => {
+            console.log(res.data)
+        }).catch((error) => {
+            console.log(error)
+        });
+
+    this.setState({ name: '', email: '',  username: '', password:'' })
+  
+}
+
 
 
     render() {
@@ -36,22 +61,22 @@ class Register extends Component {
         <div className="Register">
         <form onSubmit={this.handleSubmit}>
         <div id="radio" onChange={this.onChangeValue}>
-           <input type="radio" id="Student" name="type" value="Student" checked={this.state.id === "1"} onChange={this.handleChange}/>
-           <label for="Student">Student</label>
+        <input type="radio" id="Student" name="id" checked={this.state.id === "1"} value="1"  onChange={this.handleChange}/>
+           <label>Student</label>
       
-           <input type="radio" id="Teacher" name="type" value="Teacher" checked={this.state.id === "2"} onChange={this.handleChange}/>
-            <label for="Teacher">Teacher</label><br/>
+           <input type="radio" id="Teacher" name="id" checked={this.state.id === "2"} value="2" onChange={this.handleChange} />
+            <label>Teacher</label>
             </div>
 
             <label className="label"  for="name"  id="name" >Name: </label>
             <input className="input" type="text" id="name" name="name"  value={this.state.name} onChange={this.handleChange}/><br/>
-            <label className="label"  for="uname"  id="uname" >Username: </label>
-            <input  className="input" type="text" id="uname" name="uname" value={this.state.username} onChange={this.handleChange}/><br/>
-            <label className="label"  for="password"  id="uname">Password: </label>
-            <input  className="input" type="password" id="password" name="password"value={this.state.pass} onChange={this.handleChange}/><br/>
+            <label className="label"  for="username"  id="username" >Username: </label>
+            <input  className="input" type="text" id="username" name="username" value={this.state.username} onChange={this.handleChange}/><br/>
+            <label className="label"  for="password"  id="password">Password: </label>
+            <input  className="input" type="password" id="password" name="password"value={this.state.password} onChange={this.handleChange}/><br/>
             <label className="label"  for="email"  id="email"  >Email: </label>
             <input  className="input" type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange}/><br/>
-            <button type="submit" className="msgBtn" onClick={this.routeChange}>Register</button>
+            <button type="submit" className="msgBtn" onClick={this.onSubmit}>Register</button>
             </form>
             </div>
       
