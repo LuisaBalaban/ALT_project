5cc9bc8a35d4ed89c9afb22ec6bc72f49a9ae16e
LuisaBalaban <67561596+LuisaBalaban@users.noreply.github.com>
2021-01-16 22:58:39 +0200

    'allReqsFinished'

--- frontend/src/containers/FeedbackT.js
+++ frontend/src/containers/FeedbackT.js
@@ -17,28 +17,63 @@ class FeedbackT extends React.Component
             activity_code:this.props.location.state.activity_code,
             description:this.props.location.state.description,
             type_id:"",
-            date:"",
-            message: "",
-            items: []
-      
+            date: new Date(),
+            totalOK:"",
+            totalAmazing:"555",
+            totalInteresting:"",
+            totalBoring:""
           }
-      
+         
+          
 
     }
+      
+
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+
+    
+  tick() {
+    const enteredCode = {
+      activity_code: this.state.activity_code 
+  }
+    
+  var self = this;
+    axios.post('http://localhost:3000/api/feedback/getFeedbackCountByActivityCode',enteredCode).then(response => 
+    {
+      self.setState={
+        totalAmazing: response.data.data.totalAmazing,
+        totalOK: response.data.data.totalOK,
+        totalBoring: response.data.data.totalBoring,
+        totalInteresting: response.data.data.totalInteresting
+      }
+      // console.log(response)
+      // console.log(response.data.data.totalAmazing)
+      }).catch(error => {
+ 
+  console.log(error.response)
+})
+
+console.log(this.state.totalAmazing)
+  }
     render() {
-        return (<div>  
+      const { totalAmazing, totalOK, totalInteresting, totalBoring } = this.state;
+        return (
+        <div>  
            <button className="codes">Codes</button>
-           <p className="name">{this.state.username}</p>
-           <table>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
+           <h3>{this.state.totalAmazing}</h3>
+           <h3>{this.state.totalOK}</h3>
+           <h3>{this.state.totalInteresting}</h3>
+           <h3>{this.state.totalBoring}</h3>
+           <h3>{this.state.date.toLocaleTimeString()}</h3>
         </div>)
     }
 }
